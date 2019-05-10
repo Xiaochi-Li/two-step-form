8f1de94122f10e000ebfcdbfe9dd83db8b0bd1b2
Xiaochi <294188719@qq.com>
2019-05-10 17:59:29 +1000

    add progress bar

---
+++ app.js
@@ -0,0 +1,5 @@
+const handleClickOnNext = () => {
+  document.getElementById('form-step-one').style.display = 'none'
+  document.getElementById('form-step-two').style.display = 'block'
+  document.getElementById('progress-bar-step').style.width = '100%'
+}
